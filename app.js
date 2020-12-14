4ddc1aaa62f42c1560d2a664de6e89a37f652428
Shashi Kumar M <shashi.mani@mountblue.tech>
2020-12-14 23:21:05 +0530

    Remove console.log statements

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 
 const toggleButton = document.querySelector('#toggle-btn');
-const cardOnePrice = document.querySelector('.price-value');
+const cardOnePrice = document.querySelector('.price-value-one');
 const cardTwoPrice = document.querySelector('.price-value-two');
 const cardThreePrice = document.querySelector('.price-value-three');
-
-console.log(cardOnePrice.innerText);
-console.log(cardTwoPrice.innerText);
-console.log(cardThreePrice.innerText);
 
 toggleButton.addEventListener('change', (event) => {
 
