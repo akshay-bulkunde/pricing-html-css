2a6c208d0d225fe0a887b4d884cae29b2c38fc7b
Shashi Kumar M <shashi.mani@mountblue.tech>
2020-12-14 21:43:32 +0530

    Add event listener to change price value in the cards

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const toggleButton = document.querySelector('#toggle-btn');
+const cardOnePrice = document.querySelector('.price-value');
+const cardTwoPrice = document.querySelector('.price-value-two');
+const cardThreePrice = document.querySelector('.price-value-three');
+
+console.log(cardOnePrice.innerText);
+console.log(cardTwoPrice.innerText);
+console.log(cardThreePrice.innerText);
+
+toggleButton.addEventListener('change', (event) => {
+
+    if(toggleButton.checked === true) {
+        cardOnePrice.innerText = 199.9;
+        cardTwoPrice.innerText = 249.99;
+        cardThreePrice.innerText = 399.99;
+    } else {
+        cardOnePrice.innerText = 19.9;
+        cardTwoPrice.innerText = 24.99;
+        cardThreePrice.innerText = 39.99;
+    }
+});
