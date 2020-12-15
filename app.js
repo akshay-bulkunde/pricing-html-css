5b39be702cfcf67ebb62fd17a5f9681d9c02ae40
Shashi Kumar M <shashi.mani@mountblue.tech>
2020-12-15 10:49:54 +0530

    Update toggle event listener to add initial update

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const cardOnePrice = document.querySelector('.price-value-one');
 const cardTwoPrice = document.querySelector('.price-value-two');
 const cardThreePrice = document.querySelector('.price-value-three');
 
-toggleButton.addEventListener('change', (event) => {
-
+function updatePriceUsingToggle() {
+    
     if(toggleButton.checked === false) {
+
         cardOnePrice.innerText = 199.9;
         cardTwoPrice.innerText = 249.99;
         cardThreePrice.innerText = 399.99;
     } else if(toggleButton.checked === true) {
+        
         cardOnePrice.innerText = 19.99;
         cardTwoPrice.innerText = 24.99;
         cardThreePrice.innerText = 39.99;
     }
+}
+
+updatePriceUsingToggle();
+
+toggleButton.addEventListener('change', (event) => {
+
+    updatePriceUsingToggle();
 });
